46c8f6cd7eac08a6aca2574fdbeb226ef9601f55
Leandro Rocha <leandro.aerolito@gmail.com>
2024-02-28 23:09:57 -0300

    estilização no <Text> da Home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,7 +4,8 @@ import {View, Text, StyleSheet} from 'react-native';
 export default function Home (){
     return (
         <View style={styles.container}>
-            <Text>Página Home</Text>
+            <Text style={styles.text}>Página Home</Text>
+            <Text style={styles.subText}>Aqui eu vou adicionar mais texto para que a área de leitura seja maior. Ou, pelo menos, para testar o quanto ficaria maneiro ter <Text style={styles.bold}>algo a mais para ler</Text>, do que apenas um título magrelo e solitário.</Text>
         </View>
     );
 }
@@ -16,5 +17,26 @@ const styles = StyleSheet.create ({
         flex: 1,
         justifyContent: "center",
         alignItems:"center",
-    }
+    },
+
+    text:{
+        color: "#f64348",
+        fontSize: 30,
+        fontWeight: "bold",
+        paddingVertical: 30,
+    },
+    subText:{
+        fontSize: 18,
+        fontWeight:"normal",
+        marginLeft: 15,
+        marginRight: 15,
+        // paddingBottom: 10,
+        padding: 50,
+        textAlign:"justify",
+        
+    },
+     bold: {
+        color: "#065425",
+        fontWeight: "bold",
+     }
 })
